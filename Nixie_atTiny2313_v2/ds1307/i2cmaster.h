cf7e469d29b30563af0df6ac03cc5955ea6433ab
Buxxter <mmm@buxxter.ru>
2016-02-15 02:01:46 +0300

    i2c Commented for unused procedures less bytes at memory

--- Nixie_atTiny2313_v2/ds1307/i2cmaster.h
+++ Nixie_atTiny2313_v2/ds1307/i2cmaster.h
@@ -127,7 +127,7 @@ extern unsigned char i2c_start(unsigned char addr);
  @retval  0 device accessible
  @retval  1 failed to access device
  */
-extern unsigned char i2c_rep_start(unsigned char addr);
+//extern unsigned char i2c_rep_start(unsigned char addr);
 
 
 /**
@@ -137,7 +137,7 @@ extern unsigned char i2c_rep_start(unsigned char addr);
  @param    addr address and transfer direction of I2C device
  @return   none
  */
-extern void i2c_start_wait(unsigned char addr);
+//extern void i2c_start_wait(unsigned char addr);
 
  
 /**
